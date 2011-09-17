820a0dd0ef5b3d100e665f77e21b655199da27ec
apple <apple@applematoMacBook-Air.local>
2011-09-18 00:47:31 +0800

    增加新的XCode4模版

---
+++ BalsamiqXmlReader/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  BalsamiqXmlReader
+//
+//  Created by apple on 11-9-18.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ BalsamiqXmlReader/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BalsamiqXmlReader
+//
+//  Created by apple on 11-9-18.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
